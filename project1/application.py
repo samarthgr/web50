906894e8c3c9ce6277e8ee710e36464e63f40626
samarthgr <samarthgiripura@gmail.com>
2018-08-25 18:38:27 +0000

    Adding books, book<isbn_num>, review routes

--- project1/application.py
+++ project1/application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session, render_template, request, redirect, jsonify
+from flask import Flask, session, render_template, request, redirect, jsonify, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -10,8 +10,8 @@ from helpers import apology, login_required
 app = Flask(__name__)
 
 # Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+#if not os.getenv("DATABASE_URL"):
+#    raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -19,8 +19,8 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+#engine = create_engine(os.getenv("DATABASE_URL"))
+#db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
@@ -125,3 +125,49 @@ def search():
 
     return jsonify({})
 
+
+@app.route("/books")
+def book():
+    """ List of books """
+
+    # Query db for 10 books [For now].
+    # We can change it to Most popular, recent or most commented
+        # TODO
+
+    # Collect the required info and pass it to html
+
+    return render_template("building.html")
+
+
+@app.route("/book/<int:isbn_number>")
+def details(isbn_number):
+    """ Details of book """
+
+    # Query the book details from db
+        # Check if book exists, if not error out
+
+        # if exists, get details
+            # TODO
+
+    # Pass it to frontend
+
+    return render_template("building.html")
+
+
+@app.route("/review", methods=["POST"])
+def review():
+    """ Store submitted review of book """
+
+    # Ensure isbn_number is submitted
+    if not request.form.get("isbn_number"):
+        return apology("Invalid book", 403)
+
+    # Ensure review is submitted
+    if not request.form.get("review"):
+        return apology("Text is not submitted", 403)
+
+    # Check if book exist, if not error out
+
+    # add review to db
+
+    return redirect(url_for(details, isbn_number=request.form.get("isbn_number")))
